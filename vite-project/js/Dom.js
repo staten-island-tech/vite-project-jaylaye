92ea3b6ce9bc27db558878b322cbb757a2d91478
jaylaye <yejayla69@gmail.com>
2023-11-22 01:01:59 -0500

    kms

---
+++ vite-project/js/Dom.js
@@ -0,0 +1,15 @@
+const DOMselectors = {
+    button1: document.getElementById("btn1"),
+    button2: document.getElementById("btn2"),
+    button3: document.getElementById("btn3"),
+    button4: document.getElementById("btn4"),
+    button5: document.getElementById("btn5"),
+    button6: document.getElementById("btn6"),
+    button7: document.getElementById("btn7"),
+    button8: document.getElementById("btn8"),
+    button9: document.getElementById("btn9"),
+    button10: document.getElementById("btn10"),
+    button11: document.getElementById("btn11"),
+    box: document.getElementById("cardContainer"),
+};
+export { DOMselectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,26 +1,34 @@
 import "../styles/style.css";
-// import {menu} from 'aos';
-// import {Domselectors} from "./Dom"
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from '../styles/counter.js'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { menu } from "./menu";
+import { DOMselectors } from "./Dom";
 
-document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+AOS.init();
 
-setupCounter(document.querySelector('#counter'))
+document.addEventListener("DOMContentLoaded", function () {
+    const appetizersButton = document.getElementById("btn");
+    appetizersButton.addEventListener("click", function () {
+        const appetizers = menu.filter(item => item.appetizer);
+        console.log(appetizers);
+    });
+    const buttons = {
+        createCards: function () {
+          menu.forEach((item) => {
+            DOMselectors.box.insertAdjacentHTML(
+              "beforeend",
+              `<div class="card" data-aos="flip-up">
+                <p>${item.foodName}</p>
+                <img src="${item.image}" alt="${item.foodName}">
+                <p>$${item.price}</p> 
+              </div>`
+            );
+          });
+        },
+      };
+    },
+)
+
+
+DOMselectors.button1.addEventListener("click", buttons.createCards);
+
