8fa159bc0c1e1d4efb45b8221d46da43bd9db3e1
jaylaye <yejayla69@gmail.com>
2023-11-22 09:16:20 -0500

    .

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,6 @@
 import "../styles/style.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
 import { menu } from "./menu";
 import { DOMselectors } from "./Dom";
 
