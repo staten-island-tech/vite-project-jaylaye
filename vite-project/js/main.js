ee4e96ceedac5520eabefb1e9205f28fe80841fd
jaylaye <yejayla69@gmail.com>
2023-11-27 09:26:47 -0500

    fk

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,31 +4,19 @@ import "../styles/style.css";
 import { menu } from "./menu";
 import { DOMselectors } from "./Dom";
 
-AOS.init();
+// AOS.init();
 
-document.addEventListener("DOMContentLoaded", function () {
-    const appetizersButton = document.getElementById("btn");
-    appetizersButton.addEventListener("click", function () {
-        const appetizers = menu.filter(item => item.appetizer);
-        console.log(appetizers);
-    });
-    const buttons = {
-        createCards: function () {
-          menu.forEach((item) => {
-            DOMselectors.box.insertAdjacentHTML(
-              "beforeend",
-              `<div class="card" data-aos="flip-up">
-                <p>${item.foodName}</p>
-                <img src="${item.image}" alt="${item.foodName}">
-                <p>$${item.price}</p> 
-              </div>`
-            );
-          });
-        },
-      };
-    },
-)
-
-
-DOMselectors.button1.addEventListener("click", buttons.createCards);
-
+function clearCards(){
+  const container = DOMselectors.container;
+  container.innerHTML = '';
+}
+function foods(){
+  clearCards();
+  const appetizers = menu.filter((menu) => menu.type===
+  <div class="card">
+  <img class="card-img" src="${menu.image}" alt="">
+    <h2>${menu.foodName}</h2>
+  <p>${menu.price}</p>
+  ')
+  );
+  }
